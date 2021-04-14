861b2db9da7441acc87387ddc1d5c45d712a1d3b
Matthew Strong <mast4878@colorado.edu>
2021-04-13 22:02:11 -0600

    example job

---
+++ sample_supercomputer_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=10:00:00
+#SBATCH --partition=sgpu      
+#SBATCH --qos=normal
+#SBATCH --output=sample-%j.out
+
+module load singularity/3.6.4
+
+# the below line is an example
+singularity exec --nv --bind /projects/mast4878/shiro-corl-2021:/shiro-corl-2021 shiro.simg bash /shiro-corl-2021/run_container.sh
